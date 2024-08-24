0213830dba0054ff22af41a50be6a862b4e96633
beenow011 <abhinavnb11@gmail.com>
2024-08-24 16:40:59 +0530

    skillpart

---
+++ src/cards/languages.card.tsx
@@ -0,0 +1,77 @@
+export const langCard = [
+    {
+        description: "JavaScript",
+        title: "The Web's Language",
+        src: "/skills/jsx.png",
+        ctaText: "Explore",
+        ctaLink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        content: () => {
+            return (
+                <ul>
+                    <li>Experience: [X years/months]</li>
+                    <li>Knowledge Level: [Beginner/Intermediate/Advanced]</li>
+                </ul>
+            );
+        },
+    },
+    {
+        description: "TypeScript",
+        title: "JavaScript with Superpowers",
+        src: "/skills/ts.png",
+        ctaText: "Explore",
+        ctaLink: "https://www.typescriptlang.org/",
+        content: () => {
+            return (
+                <ul>
+                    <li>Experience: [X years/months]</li>
+                    <li>Knowledge Level: [Beginner/Intermediate/Advanced]</li>
+                </ul>
+            );
+        },
+    },
+    {
+        description: "Python",
+        title: "The Language of Simplicity",
+        src: "/skills/py.png",
+        ctaText: "Explore",
+        ctaLink: "https://www.python.org/",
+        content: () => {
+            return (
+                <ul>
+                    <li>Experience: [X years/months]</li>
+                    <li>Knowledge Level: [Beginner/Intermediate/Advanced]</li>
+                </ul>
+            );
+        },
+    },
+    {
+        description: "C++",
+        title: "The Powerhouse of Performance",
+        src: "/skills/cpp.png",
+        ctaText: "Explore",
+        ctaLink: "https://isocpp.org/",
+        content: () => {
+            return (
+                <ul>
+                    <li>Experience: [X years/months]</li>
+                    <li>Knowledge Level: [Beginner/Intermediate/Advanced]</li>
+                </ul>
+            );
+        },
+    },
+    {
+        description: "C Programming",
+        title: "The Foundation of Modern Programming",
+        src: "/skills/cprog.png",
+        ctaText: "Explore",
+        ctaLink: "https://en.wikipedia.org/wiki/C_(programming_language)",
+        content: () => {
+            return (
+                <ul>
+                    <li>Experience: [X years/months]</li>
+                    <li>Knowledge Level: [Beginner/Intermediate/Advanced]</li>
+                </ul>
+            );
+        },
+    },
+];
